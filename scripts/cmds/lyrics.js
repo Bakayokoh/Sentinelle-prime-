250567e28b95da271f14fa6278a0c3c1c9256834
Bakayokoh <raimesrtimyz@gmail.com>
2024-07-01 01:52:00 +0000

    Update lyrics.js

--- scripts/cmds/lyrics.js
+++ scripts/cmds/lyrics.js
@@ -1,47 +1,85 @@
 const axios = require("axios");
 
+
+
 module.exports = {
-  config: {
-    name: "lyrics",
-    version: "1.0",
-    author: "Aryan Chauhan",
-    countDown: 0,
-    role: 0,
-    shortDescription: {
-      en: "Get lyrics for a song",
-    },
-    longDescription: {
-      en: "This command allows you to get the lyrics for a song. Usage: !lyrics <song name>",
-    },
-    category: "music",
-    guide: {
-      en: "{p}{n} <song name>",
-    },
-  },
 
-  onStart: async function ({ api, event, args }) {
-    const songName = args.join(" ");
-    if (!songName) {
-      api.sendMessage("⛔ 𝗜𝗡𝗩𝗔𝗟𝗜𝗗 𝗧𝗜𝗧𝗟𝗘\n\n💕 Please Provide A Song Name!", event.threadID, event.messageID);
-      return;
-    }
+ config: {
 
-    const apiUrl = `https://aryanapis.replit.app/lyricsx?songName=${encodeURIComponent(songName)}`;
-    try {
-      const response = await axios.get(apiUrl);
-      const { lyrics, title, artist, image } = response.data;
-      if (!lyrics) {
-        api.sendMessage("⛔ 𝗡𝗢𝗧 𝗙𝗢𝗨𝗡𝗗\n\n😸 Sorry, lyrics not found!", event.threadID, event.messageID);
-        return;
-      }
-      let message = `📌 𝗛𝗘𝗥𝗘 𝗜𝗦 𝗟𝗬𝗥𝗜𝗖𝗦\n\n🎧 𝗧𝗜𝗧𝗟𝗘\n➪ ${title}\n👑 𝗔𝗥𝗧𝗜𝗦𝗧 \n➪ ${artist} \n\n🎶 𝗟𝗬𝗥𝗜𝗖𝗦\n➪ ${lyrics}`;
-      let attachment = await global.utils.getStreamFromURL(image);
-      api.sendMessage({ body: message, attachment }, event.threadID, (err, info) => {
-        let id = info.messageID;
-      });
-    } catch (error) {
-      console.error(error);
-      api.sendMessage("Sorry, there was an error getting the lyrics!", event.threadID, event.messageID);
-    }
-  },
+ name: "lyrics",
+
+ version: "1.0",
+
+ author: "modified by yukinori",
+
+ countDown: 5,
+
+ role: 0,
+
+ shortDescription: {
+
+ en: "Get lyrics for a song",
+
+ },
+
+ longDescription: {
+
+ en: "This command allows you to get the lyrics for a song. Usage: !lyrics <song name>",
+
+ },
+
+ category: "music",
+
+ guide: {
+
+ en: "{prefix}lyrics <song name>",
+
+ },
+
+ },
+
+
+
+ onStart: async function ({ api, event, args }) {
+
+ const songName = args.join(" ");
+
+ if (!songName) {
+
+ api.sendMessage("Please provide a song name!", event.threadID, event.messageID);
+
+ return;
+
+ }
+
+
+
+ const apiUrl = `https://lyrist.vercel.app/api/${encodeURIComponent(songName)}`;
+
+ try {
+
+ const response = await axios.get(apiUrl);
+
+ const lyrics = response.data.lyrics;
+
+ if (!lyrics) {
+
+ api.sendMessage("Sorry, lyrics not found!", event.threadID, event.messageID);
+
+ return;
+
+ }
+
+ api.sendMessage(lyrics, event.threadID, event.messageID);
+
+ } catch (error) {
+
+ console.error(error);
+
+ api.sendMessage("Sorry, there was an error getting the lyrics!", event.threadID, event.messageID);
+
+ }
+
+ },
+
 };
